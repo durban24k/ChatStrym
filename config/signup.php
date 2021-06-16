a2ad638346cdc76c3f86e5c8a8d8f1c7ed22c761
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2021-06-16 19:23:27 +0300

    mvp dev

--- config/signup.php
+++ config/signup.php
@@ -9,7 +9,19 @@
      if(!empty($fname) && !empty($lname) && !empty($email) && !empty($pswrd)){
           // Validate email address
           if(filter_var($email,FILTER_VALIDATE_EMAIL)){
+               $sql=mysqli_query($conn,"SELECT email FROM users WHERE email = '{$email}'");
+               if(mysqli_num_rows($sql)>0){
+                    echo "$email already exists!";
+               }else{
+                    $unique_id=mt_rand();
+                    $status="Active Now";
+                    $timestamp=time();
+                    $sql2=mysqli_query($conn,"INSERT INTO users (unique_id,fname,lname,email,pswrd,img,login_status,created_at,updated_at) 
+                                      VALUES ({$unique_id},'{$fname}','{$lname}','{$email}','{$pswrd}','{$img}','{$status}','{$created_at}','{$updated_at}'");
+                    if($sql2){
 
+                    }
+               }
           }else{
                echo "Email not valid";
           }
