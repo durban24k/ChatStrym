87a4c1bce1401c9c15c8d017314bdfd150c08648
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2021-06-16 20:28:46 +0300

    mvp dev

--- user/index.php
+++ user/index.php
@@ -2,14 +2,62 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ChatStrym | Client</title>
-    <link rel="stylesheet" href="./css/client.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <link rel="stylesheet" href="./css/client.css" type="text/css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 </head>
 <body>
-     <button class="live-help" id="openChat" onclick="openChat()"><i class="fas fa-comment-alt"></i></button>
+     <div class="contact-links">
+          <div class="link">
+               <a href="#"><i class="fas fa-map-marker-alt"></i> USIU Rd, Nairobi</a>
+          </div>
+          <div class="link">
+               <a href="#"><i class="fas fa-envelope-open"></i> info@chatstrym.app</a>
+          </div>
+          <div class="link">
+               <a href="#"><i class="fas fa-phone-volume"></i> +254(0)700 000000</a>
+          </div>
+     </div>
+     <nav>
+          <div class="logo">
+               <a href="#">ChatStrym</a>
+          </div>
+          <label for="btn" class="icon"><i class="fas fa-bars"></i></label>
+          <input type="checkbox" id="btn">
+          <ul>
+               <li>
+                    <label for="btn-1" class="show">Products <i class="fas fa-caret-down"></i></label>
+                    <a href="#">Products <i class="fas fa-caret-down"></i></a>
+                    <input type="checkbox" id="btn-1">
+                    <ul>
+                         <li><a href="#">LiveChat</a></li>
+                         <li><a href="#">Chatbots</a></li>
+                    </ul>
+               </li>
+               <li><a href="#">Pricing</a></li>
+               <li>
+                    <label for="btn-3" class="show">Support <i class="fas fa-caret-down"></i></label>
+                    <a href="#">Support <i class="fas fa-caret-down"></i></a>
+                    <input type="checkbox" id="btn-2">
+                    <ul>
+                         <li><a href="#">Knowledge Base</a></li>
+                         <li><a href="#">Integration</a></li>
+                    </ul>
+               </li>
+               <li><a href="#">Demo</a></li>
+               <li><a href="#" class="freetrial">Free Trial</a></li>
+               <li><a href="#" class="lp-login">Log In</a></li>
+          </ul>
+     </nav>
+     <div class="hero">
+          
+     </div>
+
+     <!-- BUTTON -->
+     <button class="live-help bounce" id="openChat" onclick="openChat()"><i class="fas fa-comment-alt"></i></button>
      <button class="live-help" id="closeChat" onclick="closeChat()"><i class="fas fa-chevron-down"></i></button>
      <span class="tooltip" id="tooltip">Welcome, Click To Chat!</span>
      <div class="wrapper" id="chatWidget">
