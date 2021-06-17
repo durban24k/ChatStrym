2d27b8370ea562cbc101e10a3d72aa849da783fe
Wanjau Alfred Mambo <56720300+durban24k@users.noreply.github.com>
2021-06-17 17:01:34 +0300

    mvp dev

--- login/index.php
+++ login/index.php
@@ -9,26 +9,39 @@
      <script src="https://kit.fontawesome.com/3637a395ce.js" crossorigin="anonymous"></script>
 </head>
 <body>
-     <div class="wrapper">
-          <section class="form login">
-               <header>ChatStrym <span>|</span> Login</header>
-               <form action="#">
-                    <div class="error-txt">This is an error message!!</div>
-                    <div class="field input">
-                         <label>Email</label>
-                         <input type="email" name="email" placeholder="Enter Your Email Address">
-                    </div>
-                    <div class="field input">
-                         <label>Password</label>
-                         <input type="password" name="password" placeholder="Enter Your Password">
-                         <i class="fas fa-eye"></i>
-                    </div>
-                    <div class="field button">
-                         <input type="submit" name="login" value="Login">
-                    </div>
-               </form>
-               <div class="link">Not Yet Registered? <a href="./register.php">Signup Now</a></div>
-          </section>
+     <div class="hero">
+          <div class="hero-img">
+               <img src="./images/login.svg" alt="">
+          </div>
+          <div class="hero-login">
+               <div class="wrapper">
+                    <section class="form login">
+                         <header>ChatStrym <span>|</span> Login</header>
+                         <form action="#">
+                              <div class="error-txt">This is an error message!!</div>
+                              <div class="field input">
+                                   <label>Email</label>
+                                   <input type="email" name="email" placeholder="Enter Your Email Address">
+                              </div>
+                              <div class="field input">
+                                   <label>Password</label>
+                                   <input type="password" name="password" placeholder="Enter Your Password">
+                                   <i class="fas fa-eye"></i>
+                              </div>
+                              <div class="field button">
+                                   <input type="submit" value="&#xf0e0;  Login With Email">
+                              </div>
+                              <div class="field forgot-pass">
+                                   <a href="#">Forgot Password?</a>
+                              </div>
+                              <div class="field google">
+                                   <input type="submit" value="&#xf1a0;  Login With Google">
+                              </div>
+                         </form>
+                         <div class="link">Not Yet Registered? <a href="./register.php">Signup Now</a></div>
+                    </section>
+               </div>
+          </div>
      </div>
      <script src="./js/pass-show.js"></script>
 </body>
--- login/register.php
+++ login/register.php
@@ -4,41 +4,51 @@
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>ChatStrym | Signup</title>
+     <title>ChatStrym | Register</title>
      <link rel="stylesheet" href="./css/style.css">
      <script src="https://kit.fontawesome.com/3637a395ce.js" crossorigin="anonymous"></script>
 </head>
 <body>
-     <div class="wrapper">
-          <section class="form signup">
-               <header>ChatStrym <span>|</span> Sign Up</header>
-               <form action="#">
-                    <div class="error-txt">This is an error message!!</div>
-                    <div class="name-details">
-                         <div class="field input">
-                              <label>First Name</label>
-                              <input type="text" name="fname" placeholder="First Name" required>
-                         </div>
-                         <div class="field input">
-                              <label>Last Name</label>
-                              <input type="text" name="lname" placeholder="Last Name" required>
-                         </div>
-                    </div>
-                    <div class="field input">
-                         <label>Email</label>
-                         <input type="email" name="email" placeholder="Enter Your Email Address" required>
-                    </div>
-                    <div class="field input">
-                         <label>Password</label>
-                         <input type="password" name="pswrd" placeholder="Enter New Password" required>
-                         <i class="fas fa-eye"></i>
-                    </div>
-                    <div class="field button">
-                         <input type="submit" value="Register">
-                    </div>
-               </form>
-               <div class="link">Already Registered? <a href="./">Login</a></div>
-          </section>
+     <div class="hero">
+          <div class="hero-img">
+               <img src="./images/register.svg" alt="">
+          </div>
+          <div class="hero-login">
+               <div class="wrapper">
+                    <section class="form signup">
+                         <header>ChatStrym <span>|</span> Register</header>
+                         <form action="#">
+                              <div class="error-txt">This is an error message!!</div>
+                              <div class="name-details">
+                                   <div class="field input">
+                                        <label>First Name</label>
+                                        <input type="text" name="fname" placeholder="First Name" required>
+                                   </div>
+                                   <div class="field input">
+                                        <label>Last Name</label>
+                                        <input type="text" name="lname" placeholder="Last Name" required>
+                                   </div>
+                              </div>
+                              <div class="field input">
+                                   <label>Email</label>
+                                   <input type="email" name="email" placeholder="Enter Your Email Address" required>
+                              </div>
+                              <div class="field input">
+                                   <label>Password</label>
+                                   <input type="password" name="pswrd" placeholder="Enter New Password" required>
+                                   <i class="fas fa-eye"></i>
+                              </div>
+                              <div class="field button">
+                                   <input type="submit" value="&#xf0e0;  Signup With Email">
+                              </div>
+                              <div class="field google">
+                                   <input type="submit" value="&#xf1a0;  Signup With Google">
+                              </div>
+                         </form>
+                         <div class="link">Already Registered? <a href="./">Login</a></div>
+                    </section>
+               </div>
+          </div>
      </div>
      <script src="./js/pass-show.js"></script>
      <script src="../js/signup.js"></script>
